041457bfa97bee2b7f690795d7acf55dd2653452
Arsh <q43676709@gmail.com>
2026-02-24 03:56:09 +0530

    Variable declaration and types in JS

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 console.log("Hello World!"); 
-document.write("Hello World!");
+//undefined
+
+console.log(typeof(something));
+
+//number
+let num=10;
+console.log(num);
+console.log(typeof(num));
+
+//boolean
+num =false;
+console.log(num);
+console.log(typeof(num));
+
+//string
+num="Arsh Rana"
+console.log(num);
+console.log(typeof(num));
+//null object
+num=null;
+console.log(num);
+console.log(typeof(num));
